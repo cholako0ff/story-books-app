6ea64403088a8f976d47309d9c435ccd6849be4e
Stan Cholakoff <115176115+cholako0ff@users.noreply.github.com>
2024-01-15 12:10:16 +0200

    edit icon helper

--- app.js
+++ app.js
@@ -30,14 +30,17 @@ if (process.env.NODE_ENV === 'development'){
 }
 
 //Handlebars Helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+
 
 // Handlebars
 app.engine(
     '.hbs',
     exphbs.engine({
         helpers: {
-            formatDate
+            formatDate,
+            stripTags, 
+            truncate,
         },
         defaultLayout: 'main',
         extname: '.hbs'

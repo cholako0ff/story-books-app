39923b09ebf0100093100d808dbea3fc0c732f82
Stan Cholakoff <115176115+cholako0ff@users.noreply.github.com>
2024-01-15 11:37:24 +0200

    truncate & strip tags

--- routes/stories.js
+++ routes/stories.js
@@ -24,5 +24,24 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc Show all stories
+// @route GET /stries
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+        
+        res.render('stories/index', {
+
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+        
+    }
+})
 
 module.exports = router

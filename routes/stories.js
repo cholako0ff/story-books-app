9b96ae4e9cf77ab7a40e19dc4080c637c38e6f88
Stan Cholakoff <115176115+cholako0ff@users.noreply.github.com>
2024-01-15 13:32:53 +0200

    aded user stories

--- routes/stories.js
+++ routes/stories.js
@@ -12,7 +12,7 @@ router.get('/add', ensureAuth, (req, res) => {
 })
 
 // @desc Process add form
-// @route POST /stries
+// @route POST /storis
 router.post('/', ensureAuth, async (req, res) => {
     try {
         req.body.user = req.user.id
@@ -26,27 +26,47 @@ router.post('/', ensureAuth, async (req, res) => {
 
 // @desc Show all stories
 // @route GET /stries/:id
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+      const stories = await Story.find({ status: 'public' })
+        .populate('user')
+        .sort({ createdAt: 'desc' })
+        .lean()
+  
+      res.render('stories/index', {
+        stories,
+      })
+    } catch (err) {
+      console.error(err)
+      res.render('error/500')
+    }
+})
+
+// @desc    Show single story
+// @route   GET /stories/:id
 router.get('/:id', ensureAuth, async (req, res) => {
     try {
-        let story = await Story.findById(req.params.id).populate('user').lean()
-    
-        if (!story) {
-          return res.render('error/404')
-        }
-    
-        if (story.user._id != req.user.id && story.status == 'private') {
-          res.render('error/404')
-        } else {
-          res.render('stories/show', {
-            story,
-          })
-        }
-      } catch (err) {
-        console.error(err)
-        res.render('error/404')
+      let story = await Story.findById(req.params.id)
+      .populate('user')
+      .lean()
+  
+      if (!story) {
+        return res.render('error/404')
       }
-    })
-
+  
+      if (story.user._id != req.user.id && story.status == 'private') {
+        res.render('error/404')
+      } else {
+        res.render('stories/show', {
+          story,
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      res.render('error/404')
+    }
+})
+  
 // @desc Show edit page
 // @route GET /stries/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -66,11 +86,11 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
             story,
           })
         }
-      } catch (err) {
+    } catch (err) {
         console.error(err)
         return res.render('error/500')
-      }
-    })
+    }
+})
     
     
 
@@ -120,6 +140,26 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     } catch (err) {
       console.error(err)
       return res.render('error/500')
+    }
+})
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+      const stories = await Story.find({
+        user: req.params.userId,
+        status: 'public',
+      })
+        .populate('user')
+        .lean()
+  
+      res.render('stories/index', {
+        stories,
+      })
+    } catch (err) {
+      console.error(err)
+      res.render('error/500')
     }
   })
 
